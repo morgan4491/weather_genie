9d12722da7b36c886ef3d3fc1f12e383c13ec537
morgan4491 <m.morgan4491@gmail.com>
2024-10-16 17:10:12 -0400

    pulled the refactored starter code

--- server/src/routes/api/weatherRoutes.ts
+++ server/src/routes/api/weatherRoutes.ts
@@ -1,22 +1,38 @@
-import { Router, type Request, type Response } from 'express';
-import weatherService from '../../service/weatherService';
+import { Router, Request, Response } from 'express';
 const router = Router();
 
-// import HistoryService from '../../service/historyService.js';
-// import WeatherService from '../../service/weatherService.js';
+import historyService from '../../service/historyService.js';
+import weatherService from '../../service/weatherService.js';
 
 // TODO: POST Request with city name to retrieve weather data
 router.post('/', async (req: Request, res: Response) => {
-  // TODO: GET weather data from city name
-  const weatherData = await weatherService.getWeatherForCity(req.body.cityName);
+  // Use the weatherService object to retrieve both current weather and forecast weather for the city the user searched for
+  // req.body.cityName is the city value that the user searched for in the browser form input
+  const currentData = await weatherService.getWeatherForCity(req.body.cityName);
+  // Create a variable forecastData that stores the forecast data, using the weatherService.getForecastWeatherForCity()
+  const forecastData = '';
+
+  // Create a variable weatherData that stores an array of the currentData as the first item and forecastData as the second item
+  const weatherData = [currentData, forecastData];
+  
   // TODO: save city to search history
+  // Use the historyService.addCity() to store the searched city into our db.json - use req.body.cityName to get the city the user searched for
+
+  // Send back a json response of the weatherData
   res.json(weatherData);
 });
 
-// TODO: GET search history
-router.get('/history', async (req: Request, res: Response) => {});
+// // TODO: GET search history
+router.get('/history', async (req: Request, res: Response) => {
+  // Use the historyService.getCities() to retreive the cities array of objects
 
-// * BONUS TODO: DELETE city from search history
-router.delete('/history/:id', async (req: Request, res: Response) => {});
+  // Send back a json response of the city object array
+});
+
+// // * BONUS TODO: DELETE city from search history
+router.delete('/history/:id', async (req: Request, res: Response) => {
+  // Use the historyService.removeCity() to remove a city by id 
+  // You can use req.params.id to get the id of the city the user wants to remove
+});
 
 export default router;
--- server/src/service/weatherService.ts
+++ server/src/service/weatherService.ts
@@ -1,93 +1,61 @@
 import dotenv from 'dotenv';
+// Import axios to make our http requests to the OpenWeatherMap API
 import axios from 'axios';
-
+// Allow us to pull variables from .env file with process.env
 dotenv.config();
-
-// TODO: Define an interface for the Coordinates object
-
-interface Coordinates {
-  latitude: number;
-  longitude: number;
-  cityName: string;
-  state: string;
-  country: string;
-}
-
-// TODO: Define a class for the Weather object
-
-class WeatherObject {
-
-  cityName: string;
-  temp: number;
-  wind: number;
-  humidity: number
-  date: string;
-  icon: string;
-
-  constructor(cityName: string, temp: number, wind: number, humidity: number, date: string, icon: string) {
-    this.cityName = cityName
-    this.temp = temp
-    this.wind = wind
-    this.humidity = humidity
-    this.date = date
-    this.icon = icon
-  }
-
-}
 
 // TODO: Complete the WeatherService class
 class WeatherService {
-  // TODO: Define the baseURL, API key, and city name properties
-  private baseURL?: string; // ternary opertor 
-  private API_Key?: string;
-  private cityName: string;
-  
+  // Define the baseURL, API key, and city name properties
+  baseURL: string;
+  apiKey: string;
+
   constructor() {
-    this.API_Key = process.env.API_KEY
-    this.baseURL = process.env.API_BASE_URL
-    this.cityName = ''
+    // TODO: Assign the properties baseURL and apiKey using process.env
+    this.baseURL = '';
+    this.apiKey = '';
   }
 
   // TODO: Create fetchLocationData method
-  private async fetchLocationData(query: string) {
+  async getForecastWeatherForCity(city: string) {
+    // Create the url variable using this.baseURL + '/forecast', a query parameter of 'units' set to imperial, a query parameter of 'q' set to the city parameter above and a query parameter of appid set to this.apiKey
+    const url = '';
 
-    const locationData = `${this.baseURL}/data/2.5/weather?q=${this.cityName}&appid=${this.API_Key}`;
+    // Make a fetch request to OpenWeatherMap using the url above constructed above
+    const res = await fetch(url);
 
-    fetch(locationData)
-      .then(response => response.json())
-      .then(apiResult => {
-        console.log(apiResult)
-      })
+    // Please console.log res.data so you can see how the returned data is structured - The 5 day forecast will be provided to you in an array of 40 objects. Each object represents a 3-hour segment of time. 
+
+    // return the array of of 40 weather objects
+
+    // BONUS: filter the array of 40 objects down to just objects that have a dt_txt that includes '12:00'
+    return res.data.list;
+
   }
 
+  // TODO: Complete getCurrentWeatherForCity method
+  async getCurrentWeatherForCity(city: string) {
+    // Create the url variable using this.baseURL + '/weather', a query parameter of 'units' set to imperial, a query parameter of 'q' set to the city parameter above and a query parameter of appid set to this.apiKey
+    const url = '';
 
-  // TODO: Create destructureLocationData method
-  private destructureLocationData(locationData: Coordinates): Coordinates { }
+    // Make a fetch request to OpenWeatherMap using the url above constructed above
+    const res = await fetch(url);
 
-  // TODO: Create buildGeocodeQuery method
-  private buildGeocodeQuery(): string { }
-  
-  // TODO: Create buildWeatherQuery method
-  private buildWeatherQuery(coordinates: Coordinates): string { }
-  
-  // TODO: Create fetchAndDestructureLocationData method
-  private async fetchAndDestructureLocationData() { }
-  
-  // TODO: Create fetchWeatherData method
-  private async fetchWeatherData(coordinates: Coordinates) { }
-  
-  // TODO: Build parseCurrentWeather method
-  private parseCurrentWeather(response: any) { }
-  
-  // TODO: Complete buildForecastArray method
-  private buildForecastArray(currentWeather: Weather, weatherData: any[]) { }
-  
-  // TODO: Complete getWeatherForCity method
-  async getWeatherForCity(city: string) {
-    const res = await axios.get(process.env.API_BASE_URL + `/data/2.5/forecast?q=${city}&appid=${process.env.API_KEY}`);
+    // Create a data object from the weather data you receive
+    const data = {
+      city: res.data.name, // this is completed for you as an example
+      date: '', // Use the already installed dayjs package to convert res.data.dt * 1000 into a formatted date like '10/17/2024'
+      icon: '',
+      iconDescription: '',
+      tempF: '',
+      windSpeed: '',
+      humidity: ''
+    };
 
-    return res.data;
+    // Return the custom data object of weather information
+    return data;
   }
 }
 
+// Export a constructed/instatiated object, using our WeatherService class above
 export default new WeatherService();
