dacb39ff8ef9ee490310324adc228476d1c243d2
morgan4491 <m.morgan4491@gmail.com>
2024-10-18 10:04:29 -0400

    finished HTML Routes file

--- server/src/routes/api/weatherRoutes.ts
+++ server/src/routes/api/weatherRoutes.ts
@@ -17,22 +17,28 @@ router.post('/', async (req: Request, res: Response) => {
   
   // TODO: save city to search history
   // Use the historyService.addCity() to store the searched city into our searchHistory.json - use req.body.cityName to get the city the user searched for
+  await historyService.addCity(req.body.cityName);
 
   // Send back a json response of the weatherData
   res.json(weatherData);
 });
 
 // // TODO: GET search history
-router.get('/history', async (req: Request, res: Response) => {
+router.get('/history', async (_, res: Response) => {
   // Use the historyService.getCities() to retreive the cities array of objects
+  const cities = await historyService.getCities();
 
   // Send back a json response of the city object array
+  res.json(cities);
 });
 
 // // * BONUS TODO: DELETE city from search history
 router.delete('/history/:id', async (req: Request, res: Response) => {
   // Use the historyService.removeCity() to remove a city by id 
   // You can use req.params.id to get the id of the city the user wants to remove
+  await historyService.removeCity(req.params.id);
+
+  res.status(204).send();
 });
 
 export default router;
