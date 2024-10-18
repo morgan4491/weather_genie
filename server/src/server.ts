0934a1569468750aa767d9158d3d6d12b93d173d
morgan4491 <m.morgan4491@gmail.com>
2024-10-18 15:42:05 -0400

    ready for deploy

--- server/src/server.ts
+++ server/src/server.ts
@@ -11,6 +11,7 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 // TODO: Serve static files of entire client dist folder - ie. We use app.use with express.static
+app.use(express.static('../client/dist'));
 
 // Implement middleware for parsing JSON and urlencoded form data
 app.use(express.json());
